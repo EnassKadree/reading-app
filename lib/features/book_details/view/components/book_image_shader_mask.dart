3e4978b85f4ccd16914c17e1c240513ae3e25860
saraatiah <saraatiah78@gmail.com>
2025-08-19 19:18:09 +0300

    edit book details screen

--- lib/features/book_details/view/components/book_image_shader_mask.dart
+++ lib/features/book_details/view/components/book_image_shader_mask.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-class BookImageShaderMask extends StatelessWidget {
-  const BookImageShaderMask({super.key});
+import '../../../shared/widgets/custom_network_image.dart';
 
+class BookImageShaderMask extends StatelessWidget {
+  const BookImageShaderMask({ required this.bookImage,super.key});
+ final String bookImage;
   @override
   Widget build(BuildContext context) {
     return     ShaderMask(
@@ -12,18 +14,18 @@ class BookImageShaderMask extends StatelessWidget {
           end: Alignment.bottomCenter,
           colors: [
             Colors.white.withOpacity(0.5),
-            Colors.white.withOpacity(0.05),
+            Colors.white.withOpacity(0.02),
           ],
-          stops: [0.4,0.9],
+          stops: const [0.4,0.9],
         ).createShader(bounds);
       },
       blendMode: BlendMode.dstIn,
-      child: Image.asset(
-        'assets/images/png/book 1.jpg',
+      child: CustomNetworkImage(
+        imageUrl: bookImage,
         fit: BoxFit.cover,
         width: double.infinity,
         height: double.infinity,
-      ),
+      )
     );
   }
 }
---
+++ lib/features/book_details/view/components/scrollable_description_widget.dart
@@ -0,0 +1,74 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:reading_app/core/utils/extensions/context_extension.dart';
+import 'package:reading_app/core/utils/extensions/space_extension.dart';
+import 'package:reading_app/core/utils/extensions/widget_extenstion.dart';
+import '../../../../core/utils/constants/styles_consts.dart' show StylesConsts;
+
+class ScrollableDescriptionWidget extends StatefulWidget {
+  const ScrollableDescriptionWidget({required this.description, super.key});
+
+  final String description;
+
+  @override
+  State<ScrollableDescriptionWidget> createState() =>
+      _ScrollableDescriptionWidgetState();
+}
+
+class _ScrollableDescriptionWidgetState
+    extends State<ScrollableDescriptionWidget> {
+  ScrollController scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "description".tr(),
+          style: StylesConsts.f24BoldBlack,
+        ),
+        10.spaceH,
+        Container(
+          constraints: BoxConstraints(maxHeight: 120.h),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.r),
+          ),
+          child: ScrollbarTheme(
+            data: ScrollbarThemeData(
+                minThumbLength: 12,
+                thumbColor:
+                    MaterialStateProperty.all(context.colorScheme.secondary)),
+            child: Scrollbar(
+              thumbVisibility: true,
+              interactive: true,
+              trackVisibility: true,
+              radius: Radius.circular(5.r),
+              thickness: 4.w,
+              controller: scrollController,
+              child: SingleChildScrollView(
+                controller: scrollController,
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 8.0.w),
+                  child: Text(
+                    '''${widget.description} ${widget.description} ${widget.description} ${widget.description}  ''',
+                    style: TextStyle(
+                        fontSize: 16.sp, color: Colors.grey, height: 1.6.h),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ).horizontalPadding;
+  }
+}
